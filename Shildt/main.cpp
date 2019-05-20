258867a59a7eba163142db16bb5d6ef94e2ea8e9
aleks-sh <aleks-sh@mail.ru>
2019-05-20 16:18:02 +0300

    1

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -2,8 +2,38 @@
 
 using namespace std;
 
+
+class myclass {
+	int a;
+public:
+	void set_a(int num);
+	int get_a();
+};
+
+void myclass::set_a(int num) {
+	a = num;
+}
+
+int myclass::get_a() {
+	return a;
+}
+
 int main() {
 
-	cout << "123";
+	char ch;
+	cout << "ֲגמהטעו סטלגמכû המ ץ. \n";
+	
+	do {
+		cout << ":";
+		cin >> ch;
+	} while (ch != 'x');
+
+	myclass b;
+
+	b.set_a(6);
+
+	cout << b.get_a();
+
+	
 	return 0;
 }
