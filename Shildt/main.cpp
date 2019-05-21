2e10450a297a3cc93fada94a4e7b1629f6ab2abc
Aleksey <aleks-sh@mail.ru>
2019-05-21 20:01:24 +0300

    deff

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -7,28 +7,12 @@
 
 using namespace std;
 
-class samp {
-	char s[80];
-public:
-	void show() { cout << s << "\n"; }
-	void set(char *str) { strcpy(s, str); }
-};
 
-samp input() {
-	char s[80];
-	samp str;
-	cout << "¬ведите строку: ";
-	cin >> s;
-	str.set(s);
-	return str;
-}
 
 int main()
 {
 	to1251();
-	samp ob;
-	ob = input();
-	ob.show();
+	
 	system("pause");
 	return 0;
 }
