6c691f2572099558d3d7c7a239eceb4383d981ed
aleks-sh <aleks-sh@mail.ru>
2019-05-21 14:35:11 +0300

    error????

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -1,24 +1,47 @@
 
 #include <iostream>
-#include "to1251.h"
+#include <cstring>
+#include <cstdlib>
 
+#include "to1251.h"
 using namespace std;
 
-class myclass {
-	int a, b;
+#pragma warning(disable : 4996)
+
+class strtype {
+	char *p;
+	int len;
 public:
-	void set(int i, int j) { a = i; b = j; }
-	void show() { cout << "a=" << a << "  b=" << b << "\n"; }
+	strtype(char *ptr);
+	~strtype();
+	void show();
 };
+strtype::strtype(char *ptr) {
+	len = strlen(ptr);
+	p = (char*)malloc(len + 1);
+	if (!p) {
+		cout << "Ошибка выделения памяти\n";
+		exit(1);
+	}
+	strcpy(p, ptr);
+}
+strtype::~strtype() {
+	cout << "Освобождение памяти\n";
+}
+void strtype::show() {
+	cout << p << "-длина: " << len<<"\n";
+}
 
 int main()
 {
 	to1251();
-	myclass o1, o2;
-	o1.set(10, 4);
-	o2 = o1;
-	o1.show();
-	o2.show();
+
+	char str1[] = "Это проверка";
+	char str2[] = "Мне нравится с++";
+	strtype s1(str1), s2(str2);
+	s1.show(); s2.show();
+	s2 = s1;
+	s1.show(); s2.show();
 
 	system("pause");
 	return 0;
