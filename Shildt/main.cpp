e29f550888f1da155c89857d1ff7cdefa466a56e
aleks-sh <aleks-sh@mail.ru>
2019-05-21 14:08:31 +0300

    3.1

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -1,12 +1,24 @@
-//#include "stdafx.h"
+
 #include <iostream>
 #include "to1251.h"
 
 using namespace std;
 
+class myclass {
+	int a, b;
+public:
+	void set(int i, int j) { a = i; b = j; }
+	void show() { cout << "a=" << a << "  b=" << b << "\n"; }
+};
+
 int main()
 {
 	to1251();
+	myclass o1, o2;
+	o1.set(10, 4);
+	o2 = o1;
+	o1.show();
+	o2.show();
 
 	system("pause");
 	return 0;
