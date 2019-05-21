24a29847ad83a97b5d89034ec489a15d8e46e109
aleks-sh <aleks-sh@mail.ru>
2019-05-21 15:48:20 +0300

    3.2.3

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -11,16 +11,16 @@ public:
 	void set_i(int n) { i = n; }
 	int get_i() { return i; }
 };
-void sqrt_it(samp o) {
-	o.set_i(o.get_i()*o.get_i());
-	cout << "Для копии объкта а значение i равно " << o.get_i() << "\n";
+void sqrt_it(samp *o) {
+	o->set_i(o->get_i()*o->get_i());
+	cout << "Для копии объкта а значение i равно " << o->get_i() << "\n";
 }
 
 int main()
 {
 	to1251();
 	samp a(10);
-	sqrt_it(a);
+	sqrt_it(&a);
 	cout << "но переменная a.i в функции main() не изменилась:";
 	cout << a.get_i()<<"\n";
 
