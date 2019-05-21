72163db9a54e1a0ca655be5bda02e92c7e968340
aleks-sh <aleks-sh@mail.ru>
2019-05-21 16:07:08 +0300

    3.3.1

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -1,13 +1,34 @@
-//#include "stdafx.h"
+
 #include <iostream>
+#include <cstring>
 #include "to1251.h"
 
+#pragma warning(disable : 4996)
+
 using namespace std;
+
+class samp {
+	char s[80];
+public:
+	void show() { cout << s << "\n"; }
+	void set(char *str) { strcpy(s, str); }
+};
+
+samp input() {
+	char s[80];
+	samp str;
+	cout << "¬ведите строку: ";
+	cin >> s;
+	str.set(s);
+	return str;
+}
 
 int main()
 {
 	to1251();
-
+	samp ob;
+	ob = input();
+	ob.show();
 	system("pause");
 	return 0;
 }
