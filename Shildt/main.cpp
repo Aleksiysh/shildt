d63e53a690b47065ef8ed99ccb2f0507758e4028
Aleksey <aleks-sh@mail.ru>
2019-05-22 18:42:42 +0300

    utf-8

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -1,4 +1,4 @@
-
+п»ї
 #include <iostream>
 #include "to1251.h"
 
@@ -6,29 +6,16 @@
 
 using namespace std;
 
-class samp {
-	int i, j;
-public:
-	void set_ij(int a, int b) { i = a; j = b; }
-	~samp() { cout << "деструктор " << i << endl; }
-	int get_product() { return i * j; }
-};
+void f(int *n);
 
 int main()
 {
 	to1251();
+	int i = 0;
+	f(&i);
 
-	samp *p = new samp[10];
-	for (int i = 0; i < 10; i++)
-	{
-		p[i].set_ij(i, i);
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		cout << i << " " << p[i].get_product() << endl;
-	}
-	delete[] p;
 	
+
 	system("pause");
 	return 0;
 }
