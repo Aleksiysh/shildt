11820ea30f48a2d385ce44b5cc0b9224e2bb452d
aleks-sh <aleks-sh@mail.ru>
2019-05-22 10:20:17 +0300

    3.4.1

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -7,12 +7,25 @@
 
 using namespace std;
 
-
+class samp {
+	int a;
+public:
+	void set_a(int n) { a = n;}
+	int get_a() { return a; }
+};
 
 int main()
 {
 	to1251();
-	
+	samp ob[4];
+	int i;
+	for (i = 0; i < 4; i++) {
+		ob[i].set_a(i);
+	}
+	for (i = 0; i < 4; i++) {
+		cout << ob[i].get_a();
+	}
+	cout << "\n";
 	system("pause");
 	return 0;
 }
