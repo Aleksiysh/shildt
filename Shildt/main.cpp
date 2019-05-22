1984606d7cf1af4194d609671de3b691860e09d3
Aleksey <aleks-sh@mail.ru>
2019-05-22 16:11:55 +0300

    4.2

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -8,20 +8,28 @@
 using namespace std;
 
 class samp {
-	int a;
+	int a, b;
 public:
-	samp(int n) { a = n;}
+	samp(int n, int m) { a = n; b = m; }
 	int get_a() { return a; }
+	int get_b() { return b; }
 };
 
 int main()
 {
 	to1251();
-	samp ob[4] = {-1,-2,-3,-4};
+	samp ob[4] = {
+		samp(1,2),
+		samp(3,4),
+		samp(5,6),
+		samp(7,8)
+	};
 	int i;
-	
+	samp *p;
+	p = (samp*)&ob;
 	for (i = 0; i < 4; i++) {
-		cout << ob[i].get_a();
+		cout << p->get_a() << " " << p->get_b() << "\n";
+		p++;
 	}
 	cout << "\n";
 	system("pause");
