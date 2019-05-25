3e29b2cc7b9f5cd2e174964e713a652adb7763f1
Aleksey <aleks-sh@mail.ru>
2019-05-25 14:04:15 +0300

    унарный минус, постфикс, префикс ++ --

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -6,54 +6,85 @@
 
 using namespace std;
 
-class coord {
+class point {
 	int x, y;
 public:
-	coord() { x = 0; y = 0; }
-	coord(int i, int j) { x = i, y = j; }
+	point() { x = 0; y = 0; }
+	point(int i, int j) { x = i, y = j; }
 	void get_xy(int &i, int &j) { i = x, j = y; }
 	void set_xy(int i, int j) { x = i; y = j; }
-	coord operator+(coord &ob2);
-	coord operator-(coord ob2);
-	coord operator=(coord ob2);
-	coord operator+(int i);
-	int operator==(coord &ob2);
-	int operator&&(coord &ob2);
+	point operator+(point &ob2);
+	point operator-(point ob2);
+	point operator-();
+	point operator=(point ob2);
+	point operator+(int i);
+	int operator==(point &ob2);
+	int operator&&(point &ob2);
+	point operator++();	//œ–≈‘» —
+	point operator--();	//œ–≈‘» —
+	point operator++(int notused);	//œŒ—“‘» —
+	point operator--(int notused);	//œŒ—“‘» —
 };
-coord coord::operator+(coord &ob2) {
-	coord temp;
+point point::operator+(point &ob2) {
+	point temp;
 	temp.x = x + ob2.x;
 	temp.y = y + ob2.y;
 	return temp;
 }
-coord coord::operator-(coord ob2) {
-	coord temp;
+point point::operator-(point ob2) {
+	point temp;
 	temp.x = x - ob2.x;
 	temp.y = y - ob2.y;
 	return temp;
 }
-coord coord::operator=(coord ob2) {
+point point::operator-() {
+	x *= -1;
+	y *= -1;
+	return *this;
+}
+point point::operator=(point ob2) {
 	x = ob2.x;
 	y = ob2.y;
 	return *this;
 }
-coord coord::operator+(int i) {
-	coord temp;
+point point::operator+(int i) {
+	point temp;
 	temp.x = x + i;
 	temp.y = y + i;
 	return temp;
 }
-int coord::operator==(coord &ob2) {
+int point::operator==(point &ob2) {
 	return (x == ob2.x && y == ob2.y);
 }
-int coord::operator&&(coord &ob2) {
+int point::operator&&(point &ob2) {
 	return (x && ob2.x) && (y && ob2.y);
 }
+point point::operator++() {
+	x++;
+	y++;
+	return *this;
+}
+point point::operator--() {
+	x--;
+	y--;
+	return *this;
+}
+point point:: operator++(int) {
+	x++;
+	y++;
+	return *this;
+}
+point point::operator--(int notused) {
+	x--;
+	y--;
+	return *this;
+}
+
 int main()
 {
 	to1251();
 
-	coord o1(10, 10), o2(5, 3), o3;
+	point o1(10, 10), o2(5, 3), o3;
 	int x, y;
 	o3 = o1 + o2;
 	o3.get_xy(x, y);
@@ -74,6 +105,22 @@ int main()
 	o2.set_xy(0, 0);
 	cout << (o1 && o2) << endl;
 
+	++o2;
+	o2.get_xy(x,y);
+	cout << x << " " << y << endl;
+	--o2; --o2;
+	o2.get_xy(x,y);
+	cout << x << " " << y << endl;
+
+	o2++;
+	o2.get_xy(x, y);
+	cout << x << " " << y << endl;
+	o2--; o2--;
+	o2.get_xy(x, y);
+	cout << x << " " << y << endl;
+	o2 = -o1;
+	o2.get_xy(x, y);
+	cout << x << " " << y << endl;
 	system("pause");
 	return 0;
 }
