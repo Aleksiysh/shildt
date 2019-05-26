17ee02e5f11c3e406c2a31fcdd13983f91587d51
Aleksey <aleks-sh@mail.ru>
2019-05-26 13:29:20 +0300

    private

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -1,18 +1,35 @@
 
 #include <iostream>
-#include <cstring>
 #include "to1251.h"
 
 #pragma warning(disable : 4996)
 
 using namespace std;
 
+class base {
+	int x;
+public:
+	void setx(int i) { x = i; }
+	void showx() { cout << x << endl; }
+};
 
+class derived :base {
+	int y;
+public:
+	void set_xy(int i, int j) { setx(i); y = j; }
+	void show_xy() { showx(); cout << y << endl; }
+};
 
 int main()
 {
 	to1251();
+	derived ob;
+	ob.set_xy(10,20);
 	
+
+	ob.show_xy();
+	
+
 	system("pause");
 	return 0;
 }
