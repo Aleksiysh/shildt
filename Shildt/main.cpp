8efb9b1dc66175e2cc4ffc205379d0329d64de71
Aleksey <aleks-sh@mail.ru>
2019-05-26 14:06:52 +0300

    передача в производный конструктор

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -12,15 +12,18 @@ public:
 	~base() { cout << "Деструктор базовый\n"; }
 };
 class derived :public base {
+	int j;
 public:
-	derived() { cout << "Конструктор производный\n"; }
+	derived(int n) { cout << "Конструктор производный\n"; j = n; }
 	~derived(){ cout << "Деструктор производный\n"; }
+	void showj() { cout << "j=" << j << endl; }
 };
 
 int main()
 {
 	to1251();
-	derived ob;
+	derived ob(10);
+	ob.showj();
 	//system("pause");
 	return 0;
 }
