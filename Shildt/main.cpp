6d3707270286e9731592fe0a38473ffe54b1a97e
Aleksey <aleks-sh@mail.ru>
2019-05-26 17:52:27 +0300

    переопределение cin>>

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include <cstring>
 #include "to1251.h"
 
 #pragma warning(disable : 4996)
@@ -13,18 +12,25 @@ public:
 	coord() { x = 0; y = 0; }
 	coord(int i, int j) { x = i; y = j; }
 	friend ostream &operator<<(ostream &stream, coord ob);
+	friend istream &operator>>(istream &stream, coord &ob);
 };
 ostream &operator<<(ostream &stream,coord ob){
 	stream << ob.x << "," << ob.y << endl;
 	return stream;
 }
-
+istream &operator>>(istream &stream, coord &ob) {
+	cout << "¬ведите координаты: ";
+	stream>> ob.x >> ob.y;
+	return stream;
+}
 int main()
 {
 	to1251();
 	coord a(1, 1), b(10, 23);
 
 	cout << a << b << endl;
+	cin >> a;
+	cout << a;
 
 	system("pause");
 	return 0;
