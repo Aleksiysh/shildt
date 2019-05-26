5e2c9ff35011826a49a6fde29008ac799064c6e4
Aleksey <aleks-sh@mail.ru>
2019-05-26 14:12:05 +0300

    общий аргумент для классов

--- Shildt/main.cpp
+++ Shildt/main.cpp
@@ -7,15 +7,17 @@
 using namespace std;
 
 class base {
+	int i;
 public:
-	base() { cout << "Конструктор базовый\n"; }
+	base(int n) { cout << "Конструктор базовый\n"; i = n; }
 	~base() { cout << "Деструктор базовый\n"; }
+	void showi() { cout << "i=" << i << endl; }
 };
 class derived :public base {
 	int j;
 public:
-	derived(int n) { cout << "Конструктор производный\n"; j = n; }
-	~derived(){ cout << "Деструктор производный\n"; }
+	derived(int n):base(n) { cout << "Конструктор производный\n"; j = n; }
+	~derived() { cout << "Деструктор производный\n"; }
 	void showj() { cout << "j=" << j << endl; }
 };
 
@@ -23,6 +25,7 @@ int main()
 {
 	to1251();
 	derived ob(10);
+	ob.showi();
 	ob.showj();
 	//system("pause");
 	return 0;
